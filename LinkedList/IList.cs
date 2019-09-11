014cdb44e7855beabf5688af612de5262b764fb7
Kenan Casey <kenancasey@gmail.com>
2019-09-11 10:40:12 -0500

    initial commit

---
+++ LinkedList/IList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace LinkedList
+{
+    public interface IList
+    {
+        void Prepend(int item);
+        void Append(int item);
+        void Remove(int item);
+        void InsertAfter(int existingItem, int newItem);
+        bool Contains(int item);
+        IList Reverse();
+
+        int Length { get; }
+        bool IsEmpty { get; }
+    }
+}
---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace LinkedList
+{
+    public class LinkedListNode
+    {
+        public int Data { get; set; }
+        public LinkedListNode Next { get; set; }
+
+        public LinkedListNode(int data = 0, LinkedListNode next=null)
+        {
+            Data = data;
+            Next = next;
+        }
+    }
+
+    public class LinkedList: IList
+    {
+        public LinkedListNode Head { get; set; }
+        public LinkedListNode Tail { get; set; }
+
+        public LinkedList()
+        {
+            Head = null;
+            Tail = null;
+        }
+
+        public int Length => throw new NotImplementedException();
+
+        public bool IsEmpty => Head==null;
+
+        public void Append(int item)
+        {
+            // create new node
+            LinkedListNode newNode = new LinkedListNode(item);
+
+            // make tail's next point to the new node
+            Tail.Next = newNode;
+            Tail = newNode;
+
+        }
+
+        public bool Contains(int item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InsertAfter(int existingItem, int newItem)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Prepend(int item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IList Reverse()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
